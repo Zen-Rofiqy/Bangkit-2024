e912be500ac2aea513117d0204a09e03a4c79e44
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-05 09:15:46 +0700

    Update

--- 04 Analisis Data dng Py/dashboard.py
+++ 04 Analisis Data dng Py/dashboard.py
@@ -1,7 +1,5 @@
 # https://github.com/dicodingacademy/a555-dashboard
 
-all_df.to_csv("all_data.csv", index=False)
-
 # Menyiapkan DataFrame
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -28,3 +26,84 @@ def create_daily_orders_df(df):
 def create_sum_order_items_df(df):
     sum_order_items_df = df.groupby("product_name").quantity_x.sum().sort_values(ascending=False).reset_index()
     return sum_order_items_df
+
+# menyiapkan bygender_df
+def create_bygender_df(df):
+    bygender_df = df.groupby(by="gender").customer_id.nunique().reset_index()
+    bygender_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    
+    return bygender_df
+
+# menyiapkan byage_df
+def create_byage_df(df):
+    byage_df = df.groupby(by="age_group").customer_id.nunique().reset_index()
+    byage_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    byage_df['age_group'] = pd.Categorical(byage_df['age_group'], ["Youth", "Adults", "Seniors"])
+    
+    return byage_df
+
+# menyiapkan bystate_df
+def create_bystate_df(df):
+    bystate_df = df.groupby(by="state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={
+        "customer_id": "customer_count"
+    }, inplace=True)
+    
+    return bystate_df
+
+# menghasilkan rfm_df
+def create_rfm_df(df):
+    rfm_df = df.groupby(by="customer_id", as_index=False).agg({
+        "order_date": "max", #mengambil tanggal order terakhir
+        "order_id": "nunique",
+        "total_price": "sum"
+    })
+    rfm_df.columns = ["customer_id", "max_order_timestamp", "frequency", "monetary"]
+    
+    rfm_df["max_order_timestamp"] = rfm_df["max_order_timestamp"].dt.date
+    recent_date = df["order_date"].dt.date.max()
+    rfm_df["recency"] = rfm_df["max_order_timestamp"].apply(lambda x: (recent_date - x).days)
+    rfm_df.drop("max_order_timestamp", axis=1, inplace=True)
+    
+    return rfm_df
+# load berkas all_data.csv
+all_df = pd.read_csv("all_data.csv")
+
+datetime_columns = ["order_date", "delivery_date"]
+all_df.sort_values(by="order_date", inplace=True)
+all_df.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+# --- Membuat Komponen Filter
+min_date = all_df["order_date"].min()
+max_date = all_df["order_date"].max()
+ 
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+main_df = all_df[(all_df["order_date"] >= str(start_date)) & 
+                (all_df["order_date"] <= str(end_date))]
+
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bygender_df = create_bygender_df(main_df)
+byage_df = create_byage_df(main_df)
+bystate_df = create_bystate_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+
+
