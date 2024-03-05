b769f96b5e80ea2e25a9dc6608aab6f88d0dc765
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-05 22:27:37 +0700

    Update

--- 04 Analisis Data dng Py/@Proyek-akhir/MyDashBoard.py
+++ 04 Analisis Data dng Py/@Proyek-akhir/MyDashBoard.py
@@ -1,3 +1,4 @@
+# streamlit run MyDashBoard.py
 # IMPORT
 import numpy as np
 import pandas as pd
@@ -28,3 +29,39 @@ st.write(
     Terlepas dari aplikasi dunia nyata yang menarik dari sistem berbagi sepeda, **karakteristik data** yang dihasilkan oleh sistem ini membuatnya menarik untuk penelitian. Berbeda dengan layanan transportasi lain seperti bus atau kereta bawah tanah, **durasi perjalanan**, **posisi keberangkatan** dan **kedatangan secara eksplisit** dicatat dalam sistem ini. Fitur ini mengubah sistem berbagi sepeda menjadi **jaringan sensor virtual** yang dapat digunakan untuk merasakan mobilitas di kota. Dengan demikian, diharapkan sebagian besar kejadian penting di kota dapat dideteksi melalui pemantauan data ini.
     """
 )
+st.markdown("> Data set")
+st.write(
+	"""
+    Proses penyewaan sepeda bersama sangat **berkorelasi** dengan **kondisi lingkungan** dan **musim**. Misalnya, **kondisi cuaca**, **curah hujan**, **hari dalam seminggu**, **musim**, **jam dalam sehari**, dan lain-lain dapat mempengaruhi perilaku penyewaan. Kumpulan data inti terkait dengan catatan historis selama **dua tahun** yang berkaitan dengan tahun **2011** dan **2012** dari sistem Capital Bikeshare, Washington D.C., Amerika Serikat yang tersedia untuk umum di http://capitalbikeshare.com/system-data. Kami mengumpulkan data tersebut dalam dua basis data **per jam** dan **per hari**, kemudian mengekstrak dan menambahkan **informasi cuaca** dan **musim** yang sesuai. Informasi cuaca diambil dari http://www.freemeteo.com.
+    """
+)
+st.markdown("> Karakteristik Dataset")
+st.write(
+	"""
+    Baik hour.csv dan day.csv memiliki bidang berikut, kecuali hr yang tidak tersedia di day.csv
+    * `instant`    : indeks catatan
+    * `dteday`     : tanggal
+    * `season`     : musim (1: musim semi, 2: musim panas, 3: musim gugur, 4: musim dingin)
+    * `yr`         : tahun (0: 2011, 1: 2012)
+    * `mnth`       : bulan (1 hingga 12)
+    * `hr`         : jam (0 hingga 23)
+    * `holiday`    : hari cuaca hari libur atau tidak (diambil dari http://dchr.dc.gov/page/holiday-schedule)
+    * `weekday`    : hari dalam seminggu
+    * `workingday` : jika hari tersebut bukan akhir pekan atau hari libur maka nilainya 1, jika tidak maka nilainya 0.
+    * `weathersit` : 
+        - 1: Cerah, Sedikit awan, Berawan sebagian, Berawan sebagian
+        - 2: Kabut + Mendung, Kabut + Awan pecah, Kabut + Sedikit awan, Kabut
+        - 3: Salju Ringan, Hujan Ringan + Badai Petir + Awan berserakan, Hujan Ringan + Awan berserakan
+        - 4: Hujan Lebat + Hujan Es + Badai Petir + Kabut, Salju + Kabut
+    * `temp`       : Suhu yang dinormalisasi dalam Celcius. Nilai dibagi menjadi 41 (maks)
+    * `atemp`      : Suhu perasaan yang dinormalisasi dalam Celcius. Nilai dibagi menjadi 50 (maks)
+    * `hum`        : Kelembapan yang dinormalisasi. Nilai dibagi menjadi 100 (maks)
+    * `windspeed`  : Kecepatan angin yang dinormalisasi. Nilai dibagi menjadi 67 (maks)
+    * `casual`     : jumlah pengguna biasa
+    * `registered` : jumlah pengguna terdaftar
+    * `cnt`        : jumlah total sepeda yang disewa termasuk yang kasual dan terdaftar
+    """
+)
+
+
+
