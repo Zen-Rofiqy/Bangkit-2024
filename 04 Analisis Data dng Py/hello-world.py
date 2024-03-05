ae84ea420767646fa16f8e1f48f73701f6fe579b
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-05 08:03:32 +0700

    Update

--- 04 Analisis Data dng Py/hello-world.py
+++ 04 Analisis Data dng Py/hello-world.py
@@ -1,3 +1,4 @@
+# streamlit run hello-world.py
 import streamlit as st 
 import pandas as pd
 
@@ -37,3 +38,71 @@ st.write(pd.DataFrame({
     'c2': [10, 20, 30, 40],
 }))
 
+# dataframe()
+sample_data = {
+    'name': ['John', 'Alia', 'Ananya', 'Steve', 'Ben'],
+    'age': [24, 22, 23, 25, 28],  
+    'communication_skill_score': [85, 70, 75, 90, 90],
+    'quantitative_skill_score': [80, 90, 80, 75, 70]
+}
+ 
+df = pd.DataFrame(sample_data)
+
+st.dataframe(data=df, width=500, height=150)
+
+# table()
+st.table(data=df)
+
+# metric()
+st.metric(label="Temperature", value="28 °C", delta="1.2 °C")
+
+# json()
+st.json({
+    'c1': [1, 2, 3, 4],
+    'c2': [10, 20, 30, 40],
+})
+
+
+# CHART
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+x = np.random.normal(15, 5, 250)
+ 
+fig, ax = plt.subplots()
+ax.hist(x=x, bins=15)
+st.pyplot(fig)
+
+# -----
+# WIDGET
+# TEXT INPUT
+name = st.text_input(label='Nama lengkap', value='')
+st.write('Nama: ', name)
+
+# Text Area
+text = st.text_area('Feedback')
+st.write('Feedback: ', text)
+
+# Number input
+number = st.number_input(label='Umur')
+st.write('Umur: ', int(number), ' tahun')
+
+# Date input
+date = st.date_input(label='Tanggal lahir', min_value=datetime.date(1900, 1, 1))
+st.write('Tanggal lahir:', date)
+
+# File Uploader
+uploaded_file = st.file_uploader('Choose a CSV file')
+ 
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+    st.dataframe(df)
+
+# Camera Input
+picture = st.camera_input('Take a picture')
+if picture:
+    st.image(picture)
+
+# BUTTON WIDGETS
+# Button
+    
