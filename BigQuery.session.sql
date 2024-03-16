6eeba50661696ffe2bea00193dc05a65972f8714
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-17 05:45:45 +0700

    Update

--- BigQuery.session.sql
+++ BigQuery.session.sql
@@ -1,3 +1,11 @@
+/*  1. connect with SQLTools
+    2. search BigQuery, (install the extention first)
+    3. In Authenticator*, select GCloud CLI
+    4. In Project ID*, get your BigQuery sandbox ID (https://cloud.google.com/bigquery/docs/sandbox)
+        if you haven't create any Project, then create new project
+        the Project will appear in Welcome page (https://console.cloud.google.com/)
+*/
+-- @block
 SELECT
   language,
   title,
@@ -11,4 +19,6 @@ GROUP BY
   title
 ORDER BY
   views DESC;
+
+-- @block
 
