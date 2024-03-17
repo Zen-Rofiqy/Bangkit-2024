158fbf17b1b57673f113d9a591f100a78ced298b
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-17 09:44:43 +0700

    Update

--- 05 Google Data Analytics/04 Process Data from Dirty to Clean/Hands-On Activity Clean data using SQL.sql
+++ 05 Google Data Analytics/04 Process Data from Dirty to Clean/Hands-On Activity Clean data using SQL.sql
@@ -44,6 +44,7 @@ FROM
 WHERE
   num_of_doors IS NULL;
 
+-- * update the data
 -- @block
 UPDATE
   cars.car_info
@@ -58,6 +59,7 @@ WHERE
 SELECT *
 FROM cars.car_info
 WHERE TRIM(num_of_doors) = '';
+-- see? we only update 1 row, and keep the other as missing value
 
 -- ! Identify potential errors
 -- @block
@@ -89,3 +91,34 @@ SELECT
 FROM
   cars.car_info;
 -- the maximum value in this column should be 23, not 70
+
+-- @block
+SELECT
+  MIN(compression_ratio) AS min_compression_ratio,
+  MAX(compression_ratio) AS max_compression_ratio
+FROM
+  cars.car_info
+WHERE
+  compression_ratio <> 70;
+
+-- * check how much the value = 70
+-- @block
+SELECT
+   COUNT(*) AS num_of_rows_to_delete
+FROM
+   cars.car_info
+WHERE
+   compression_ratio = 70;
+
+-- * delete the value = 70
+-- @block
+DELETE FROM cars.car_info -- yea I added 'FROM'
+WHERE compression_ratio = 70;
+
+-- ! Ensure consistency
+-- * check drive_wheels
+-- @block
+SELECT
+  DISTINCT drive_wheels
+FROM
+  cars.car_info;
