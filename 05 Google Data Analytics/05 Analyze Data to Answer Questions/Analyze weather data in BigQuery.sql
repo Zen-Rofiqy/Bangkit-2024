45cf1c62a866c348ff862b62a5ec4cacb974f032
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-17 13:22:52 +0700

    Update

--- 05 Google Data Analytics/05 Analyze Data to Answer Questions/Analyze weather data in BigQuery.sql
+++ 05 Google Data Analytics/05 Analyze Data to Answer Questions/Analyze weather data in BigQuery.sql
@@ -25,32 +25,18 @@ ORDER BY
   stn ASC
 
 -- ! Save a new table
+-- * Save to MySql (Must create `nyc_weather` schema first)
 -- @block
-CREATE OR REPLACE TABLE `my-first-project-417404.demos.nyc_weather`
-AS
-SELECT
-  stn,
-  date,
-  IF(
-     temp=9999.9,
-     NULL,
-     temp) AS temperature,
-  IF(
-     wdsp="999.9",
-     NULL,
-     CAST(wdsp AS Float64)) AS wind_speed,
-  IF(
-     prcp=99.99,
-     0,
-     prcp) AS precipitation
-FROM
-  `bigquery-public-data.noaa_gsod.gsod2020`
-WHERE
-  stn="725030" -- La Guardia
-  OR stn="744860" -- JFK
-ORDER BY
-  date DESC,
-  stn ASC;
+USE nyc_weather; -- Connect to MySQL database/schema
+
+CREATE TABLE weather_data (
+    stn VARCHAR(10),
+    date DATE,
+    temperature FLOAT,
+    wind_speed FLOAT,
+    precipitation FLOAT
+);
+
 
 
 
